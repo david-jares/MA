920c4d7b09931720bbb20cc4f413b0f9e7a865ac
David Jares <traubensaftschorle@googlemail.com>
2023-09-06 23:37:52 +0200

    canvas2 scaling , inverting coordinates and panning works

--- frontend_vue/cows/src/components/CanvasScalable.vue
+++ frontend_vue/cows/src/components/CanvasScalable.vue
@@ -1,40 +1,72 @@
 <script setup lang="ts">
-import { drawRectangle, drawPolygonToCanvas, origin, drawCoordinateSystem, drawPolygonToCanvasScaled as drawPolygonScaled, drawRectangleScaled, moveOrigin, addToScale } from '@/myfunctions/canvashelperfunctions';
+import { drawRectangle, drawPath, origin, drawCoordinateSystem, drawPathToCanvasScaled as drawPolygonScaled, drawRectangleScaled, moveOrigin, addToScale, degreeLongitudeToMeters, degreeLatitideToMeters, setOrigin, drawLine, drawLineYInv, drawCoordinateSystemYInv, getPointYInv, getPointsYInv, setFillColor, getPointsScaled, getPointsScaledYInv } from '@/myfunctions/canvashelperfunctions';
+import { useGlobalsStore } from '@/stores/globals';
 import { onMounted, ref } from 'vue';
 
 let canvas: HTMLCanvasElement | null;
 let ctx: CanvasRenderingContext2D | null;
+const gs = useGlobalsStore();
 // testfunction();
 // console.log(coordinateOrigin);
 
 onMounted(() => {
     canvas = document.getElementById("mycanvas") as HTMLCanvasElement;
     ctx = canvas.getContext("2d");
+
+    let geoCoords = gs.coordinatesCombined;
+    let geoCoords_m = geoCoords.map((coord) => {
+        return { x: coord.lon * degreeLongitudeToMeters, y: coord.lat * degreeLatitideToMeters };
+    });
+    const minX_m = Math.min(...geoCoords_m.map(coord => coord.x));
+    const maxX_m = Math.max(...geoCoords_m.map(coord => coord.x));
+    const minY_m = Math.min(...geoCoords_m.map(coord => coord.y));
+    const maxY_m = Math.max(...geoCoords_m.map(coord => coord.y));
+    let coordHeight_m = maxY_m - minY_m;
+    let coordWidth_m = maxX_m - minX_m;
+    let coordShiftFromOrigin = {x:minX_m, y:minY_m};
+    console.log("coordWidth_m: " + coordWidth_m);
+    console.log("coordHeight_m: " + coordHeight_m);
+    // setOrigin(minX_m, minY_m);
+
+    let geoCoordsShiftedBack_m = geoCoords_m.map((coord) => {
+        return { x: coord.x - coordShiftFromOrigin.x, y: coord.y - coordShiftFromOrigin.y };
+    });
+
+
+
+    console.log(geoCoords)
+    console.log(geoCoords_m)
+    console.log(geoCoordsShiftedBack_m)
+
     let interval = setInterval(() => {
         if (ctx == null || canvas == null) { return; }
         ctx?.clearRect(0, 0, canvas.width, canvas.height);
+        let testPoints = [{ x: 100, y: 0 }, { x: 200, y: 100 }, { x: 200, y: 200 }, { x: 100, y: 200 }];
 
-        drawCoordinateSystem(ctx);
-        // drawPolygonToCanvas(ctx, [{ x: 20, y: 0 }, { x: 100, y: 100 }, { x: 100, y: 200 }, { x: 20, y: 200 }], coordinateOrigin);
-        // drawRectangle(ctx, coordinateOrigin.x  +50,coordinateOrigin.y + 50, 200, 200,"rgba(0,0,0,0.25)" );
-        
-        drawPolygonScaled(ctx, [{ x: 100, y: 0 }, { x: 200, y: 100 }, { x: 200, y: 200 }, { x: 100, y: 200 }]);
-        drawRectangleScaled(ctx, 100, 100, 200, 200, "rgba(0,0,0,0.25)");
+        drawCoordinateSystemYInv(ctx);
+
+      
+        // setFillColor(ctx, "rgba(0,255,255,0.65)");
+        // drawPath(ctx, getPointsScaledYInv(ctx, testPoints));
+        setFillColor(ctx, "rgba(255,0,255,0.65)");
+        drawPath(ctx, getPointsScaledYInv(ctx, geoCoordsShiftedBack_m));
+
+        // drawRectangleScaled(ctx, 100, 100, 200, 200, "rgba(0,0,0,0.25)");
     }, 50);
 
 
     document.addEventListener("keydown", (e) => {
         if (e.key == "ArrowUp" && e.shiftKey) {
-            addToScale(0.25);
+            addToScale(0.1);
         }
         else if (e.key == "ArrowDown" && e.shiftKey) {
-            addToScale(-0.25);
+            addToScale(-0.1);
         }
         else if (e.key == "ArrowUp") {
-            moveOrigin(0, -50);
+            moveOrigin(0, 50);
         }
         else if (e.key == "ArrowDown") {
-            moveOrigin(0, 50);
+            moveOrigin(0, -50);
         }
         else if (e.key == "ArrowLeft") {
             moveOrigin(-50, 0);
--- frontend_vue/cows/src/myfunctions/canvashelperfunctions.ts
+++ frontend_vue/cows/src/myfunctions/canvashelperfunctions.ts
@@ -1,26 +1,27 @@
 import type { Point } from "./tempfunctions";
 
-export const testvalue = 42;
+// export let coordinateOrigin = { x: 0, y: 0 };
+export let origin = { x: 0, y: 0 };
+export let scale = 3;
+export const degreeLongitudeToMeters = 72186; // in Nürnberg Germany
+export const degreeLatitideToMeters = 110000; // in Nürnberg Germany
 
 //STYLING-------------------------------------------------------------------------
-function setStrokeProperties(ctx: CanvasRenderingContext2D, borderColor: string, borderThickness: number = 1): void {
+export function setStrokeProperties(ctx: CanvasRenderingContext2D, borderColor: string, borderThickness: number = 1): void {
   ctx.strokeStyle = borderColor;
   ctx.lineWidth = borderThickness;
 }
 
-function setFontProperties(ctx: CanvasRenderingContext2D, fontColor: string, fontSize: number = 12, fontFamily: string = "Arial"): void {
+export function setFontProperties(ctx: CanvasRenderingContext2D, fontColor: string, fontSize: number = 12, fontFamily: string = "Arial"): void {
   ctx.fillStyle = fontColor;
   ctx.font = `${fontSize}px ${fontFamily}`;
 }
 
-function setFillColor(ctx: CanvasRenderingContext2D, fillColor: string): void {
+export function setFillColor(ctx: CanvasRenderingContext2D, fillColor: string): void {
   ctx.fillStyle = fillColor;
 }
 //STYLING-------------------------------------------------------------------------
 
-// export let coordinateOrigin = { x: 0, y: 0 };
-export let origin = { x: 100, y: 100 };
-export let scale = 1;
 
 export function moveOrigin(x: number, y: number): void {
   origin.x += x;
@@ -34,7 +35,7 @@ export function setOrigin(x: number, y: number): void {
 export function setScale(s: number): void {
   if (s < 0.1) {
     scale = 0.1;
-  }else{
+  } else {
     scale = s;
   }
 }
@@ -42,9 +43,68 @@ export function setScale(s: number): void {
 export function addToScale(s: number): void {
   if (scale + s < 0.1) {
     scale = 0.1;
-  }else{
+  } else {
     scale += s;
   }
+}
+
+export function getYInv(ctx: CanvasRenderingContext2D, y: number): number {
+  let h = ctx.canvas.height;
+  return h - y;
+}
+
+export function getPointYInv(ctx: CanvasRenderingContext2D, p: Point): Point {
+  // let h = ctx.canvas.height;
+  let pYInv = { x: p.x, y: getYInv(ctx, p.y) };
+  return { x: pYInv.x, y: pYInv.y };
+}
+
+export function getPointsYInv(ctx: CanvasRenderingContext2D, points: Point[]): Point[] {
+  return points.map(p => getPointYInv(ctx, p));
+}
+
+export function getPointScaled(ctx: CanvasRenderingContext2D, p: Point): Point {
+  let p2: Point = { x: p.x, y: p.y };
+  p2.x *= scale;
+  p2.y *= scale;
+  p2 = { x: p2.x + origin.x, y: p2.y + origin.y };
+  return p2;
+}
+export function getPointsScaled(ctx: CanvasRenderingContext2D, points: Point[]): Point[] {
+  return points.map(p => getPointScaled(ctx, p));
+}
+
+export function getPointScaledYInv(ctx: CanvasRenderingContext2D, p: Point): Point {
+  return getPointYInv(ctx, getPointScaled(ctx, p));
+}
+
+export function getPointsScaledYInv(ctx: CanvasRenderingContext2D, points: Point[]): Point[] {
+  return points.map(p => getPointScaledYInv(ctx, p));
+}
+
+
+export function drawLine(ctx: CanvasRenderingContext2D, p1: Point, p2: Point): void {
+  ctx.beginPath();
+  ctx.moveTo(p1.x, p1.y);
+  ctx.lineTo(p2.x, p2.y);
+  ctx.stroke();
+}
+export function drawLineYInv(ctx: CanvasRenderingContext2D, p1: Point, p2: Point): void {
+  let p1Inv = getPointYInv(ctx, p1);
+  let p2Inv = getPointYInv(ctx, p2);
+  ctx.beginPath();
+  ctx.moveTo(p1Inv.x, p1Inv.y);
+  ctx.lineTo(p2Inv.x, p2Inv.y);
+  ctx.stroke();
+}
+
+export function drawText(ctx: CanvasRenderingContext2D, text: string, x: number, y: number): void {
+  ctx.fillText(text, x, y);
+}
+export function drawTextYInv(ctx: CanvasRenderingContext2D, text: string, x: number, y: number): void {
+  // let h = ctx.canvas.height;
+  let yInv = getYInv(ctx, y);
+  ctx.fillText(text, x, yInv);
 }
 
 export function drawCoordinateSystem(ctx: CanvasRenderingContext2D): void {
@@ -54,105 +114,117 @@ export function drawCoordinateSystem(ctx: CanvasRenderingContext2D): void {
   setStrokeProperties(ctx, "black", 1);
 
   // Draw the x-axis
-  ctx.beginPath();
-  ctx.moveTo(origin.x, origin.y);
-  ctx.lineTo(origin.x + ctx.canvas.width * scale, origin.y);
-  ctx.stroke();
+  drawLine(ctx, { x: origin.x, y: origin.y }, { x: origin.x + ctx.canvas.width * scale, y: origin.y });
 
   // Draw the y-axis
-  ctx.beginPath();
-  ctx.moveTo(origin.x, origin.y);
-  ctx.lineTo(origin.x, origin.y + ctx.canvas.height * scale);
-  ctx.stroke();
+  drawLine(ctx, { x: origin.x, y: origin.y }, { x: origin.x, y: origin.y + ctx.canvas.height * scale });
 
   // Draw the text
   setFontProperties(ctx, "black", 12, "Arial");
   // Draw the tick marks and labels on the x-axis
   for (let i = 0; i < ctx.canvas.width / spacing; i++) {
-    ctx.beginPath();
-    ctx.moveTo(origin.x + i * spacing * scale, origin.y - 5);
-    ctx.lineTo(origin.x + i * spacing * scale, origin.y + 5);
-    ctx.stroke();
-    ctx.fillText((i * spacing).toString(), origin.x + i * spacing * scale, origin.y + 20);
+    drawLine(ctx, { x: origin.x + i * spacing * scale, y: origin.y - 5 }, { x: origin.x + i * spacing * scale, y: origin.y + 5 });
+    drawText(ctx, (i * spacing).toString(), origin.x + i * spacing * scale, origin.y + 20);
   }
 
   // Draw the tick marks and labels on the y-axis
   for (let i = 0; i < ctx.canvas.height / spacing; i++) {
-    ctx.beginPath();
-    ctx.moveTo(origin.x - 5, origin.y + i * spacing * scale);
-    ctx.lineTo(origin.x + 5, origin.y + i * spacing * scale);
-    ctx.stroke();
-    ctx.fillText((i * spacing).toString(), origin.x + 20, origin.y + i * spacing * scale);
+    drawLine(ctx, { x: origin.x - 5, y: origin.y + i * spacing * scale }, { x: origin.x + 5, y: origin.y + i * spacing * scale });
+    drawText(ctx, (i * spacing).toString(), origin.x + 20, origin.y + i * spacing * scale);
+  }
+}
+
+export function drawCoordinateSystemYInv(ctx: CanvasRenderingContext2D): void {
+
+  let spacing = 100;
+  let h = ctx.canvas.height;
+  // Set the line color to black
+  setStrokeProperties(ctx, "black", 1);
+
+  // Draw the x-axis
+  drawLineYInv(ctx, { x: origin.x, y: origin.y }, { x: origin.x + ctx.canvas.width * scale, y: origin.y });
+
+  // Draw the y-axis
+  drawLineYInv(ctx, { x: origin.x, y: origin.y }, { x: origin.x, y: origin.y + ctx.canvas.height * scale });
+
+  // Draw the text
+  setFontProperties(ctx, "black", 12, "Arial");
+  // Draw the tick marks and labels on the x-axis
+  for (let i = 0; i < ctx.canvas.width / spacing; i++) {
+    drawLineYInv(ctx, { x: origin.x + i * spacing * scale, y: origin.y - 5 }, { x: origin.x + i * spacing * scale, y: origin.y + 5 });
+    drawTextYInv(ctx, (i * spacing).toString(), origin.x + i * spacing * scale, origin.y + 20);
+  }
+
+  // Draw the tick marks and labels on the y-axis
+  for (let i = 0; i < ctx.canvas.height / spacing; i++) {
+    drawLineYInv(ctx, { x: origin.x - 5, y: origin.y + i * spacing * scale }, { x: origin.x + 5, y: origin.y + i * spacing * scale });
+    drawTextYInv(ctx, (i * spacing).toString(), origin.x + 20, origin.y + i * spacing * scale);
   }
 }
 
 
-export function drawPolygonToCanvas(ctx: CanvasRenderingContext2D, polygon: Point[], offset: Point = { x: 0, y: 0 }): void {
-  // ctx.strokeStyle = "black";
+export function drawPath(ctx: CanvasRenderingContext2D, path: Point[], closePath: boolean = true, fillArea: boolean = true, offset: Point = { x: 0, y: 0 }): void {
 
   ctx.beginPath();
 
-  polygon.forEach((point, index) => {
+  path.forEach((point, index) => {
     let transformedPoint: Point = { x: point.x + offset.x, y: point.y + offset.y };
     if (index === 0) ctx.moveTo(transformedPoint.x, transformedPoint.y);
     else ctx.lineTo(transformedPoint.x, transformedPoint.y);
+    drawText(ctx, index.toString(), transformedPoint.x + 5, transformedPoint.y - 5);
 
-    ctx.fillText(index.toString(), transformedPoint.x + 5, transformedPoint.y - 5);
     if (index === 0) ctx.moveTo(transformedPoint.x, transformedPoint.y);
     else ctx.lineTo(transformedPoint.x, transformedPoint.y);
-
-    ctx.fillText(index.toString(), transformedPoint.x + 5, transformedPoint.y - 5);
+    drawText(ctx, index.toString(), transformedPoint.x + 5, transformedPoint.y - 5);
   });
-  ctx.closePath();
-  setFillColor(ctx, 'rgba(255, 0, 0, 0.8)');
-  ctx.fill();
+  if (closePath) ctx.closePath();
+  if (fillArea) {
+    ctx.fill();
+  }
+  else {
+    ctx.stroke();
+  }
 }
-export function drawPolygonToCanvasScaled(ctx: CanvasRenderingContext2D, polygon: Point[]): void {
-  // ctx.strokeStyle = "black";
+// export function drawPathToCanvasScaled(ctx: CanvasRenderingContext2D, path: Point[]): void {
+//   // ctx.strokeStyle = "black";
 
-  ctx.beginPath();
+//   ctx.beginPath();
 
-  polygon.forEach((point, index) => {
-    let transformedPoint: Point = { x: point.x, y: point.y };
-    transformedPoint.x *= scale;
-    transformedPoint.y *= scale;
-    transformedPoint = { x: transformedPoint.x + origin.x, y: transformedPoint.y + origin.y };
-    if (index === 0) ctx.moveTo(transformedPoint.x, transformedPoint.y);
-    else ctx.lineTo(transformedPoint.x, transformedPoint.y);
+//   path.forEach((point, index) => {
+//     let transformedPoint: Point = { x: point.x, y: point.y };
+//     transformedPoint.x *= scale;
+//     transformedPoint.y *= scale;
+//     transformedPoint = { x: transformedPoint.x + origin.x, y: transformedPoint.y + origin.y };
+//     if (index === 0) ctx.moveTo(transformedPoint.x, transformedPoint.y);
+//     else ctx.lineTo(transformedPoint.x, transformedPoint.y);
+//     drawText(ctx, index.toString(), transformedPoint.x + 5, transformedPoint.y - 5);
 
-    ctx.fillText(index.toString(), transformedPoint.x + 5, transformedPoint.y - 5);
-    if (index === 0) ctx.moveTo(transformedPoint.x, transformedPoint.y);
-    else ctx.lineTo(transformedPoint.x, transformedPoint.y);
-
-    ctx.fillText(index.toString(), transformedPoint.x + 5, transformedPoint.y - 5);
-  });
-  ctx.closePath();
-  setFillColor(ctx, 'rgba(255, 0, 0, 0.8)');
-  ctx.fill();
-}
+//     if (index === 0) ctx.moveTo(transformedPoint.x, transformedPoint.y);
+//     else ctx.lineTo(transformedPoint.x, transformedPoint.y);
+//     drawText(ctx, index.toString(), transformedPoint.x + 5, transformedPoint.y - 5);
+//   });
+//   ctx.closePath();
+//   ctx.fill();
+// }
 
 
 export function drawRectangle(ctx: CanvasRenderingContext2D, x: number, y: number, width: number, height: number, fillColor: string): void {
-  ctx.strokeStyle = 'rgba(0, 0, 0, 0.8)';
-  ctx.lineWidth = 2;
   ctx.strokeRect(x, y, width, height);
   if (fillColor) {
     ctx.fillStyle = fillColor;
     ctx.fillRect(x + 2, y + 2, width - 4, height - 4);
   }
-
 }
-export function drawRectangleScaled(ctx: CanvasRenderingContext2D, x: number, y: number, width: number, height: number, fillColor: string): void {
-  ctx.strokeStyle = 'rgba(0, 0, 0, 0.8)';
-  ctx.lineWidth = 2;
-  let x1 = origin.x + x * scale;
-  let y1 = origin.y + y * scale;
-  let w = width * scale;
-  let h = height * scale;
-  ctx.strokeRect(x1, y1, w, h);
-  if (fillColor) {
-    ctx.fillStyle = fillColor;
-    ctx.fillRect(x1 + 2, y1 + 2, w - 4, h - 4);
-  }
 
-}
+// export function drawRectangleScaled(ctx: CanvasRenderingContext2D, x: number, y: number, width: number, height: number, fillColor: string): void {
+//   let x1 = origin.x + x * scale;
+//   let y1 = origin.y + y * scale;
+//   let w = width * scale;
+//   let h = height * scale;
+//   ctx.strokeRect(x1, y1, w, h);
+//   if (fillColor) {
+//     ctx.fillStyle = fillColor;
+//     ctx.fillRect(x1 + 2, y1 + 2, w - 4, h - 4);
+//   }
+
+// }
